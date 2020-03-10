17c07bab691a6812cbb45cf2e580508bdc45230d
Brian Gottreu <gottreu@gmail.com>
2020-03-10 16:26:00 -0500

    Add User and UserDAO classes

---
+++ src/main/java/com/revature/models/User.java
@@ -0,0 +1,91 @@
+package com.revature.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private boolean canComment;
+	private boolean isBanned;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isCanComment() {
+		return canComment;
+	}
+	public void setCanComment(boolean canComment) {
+		this.canComment = canComment;
+	}
+	public boolean isBanned() {
+		return isBanned;
+	}
+	public void setBanned(boolean isBanned) {
+		this.isBanned = isBanned;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (canComment ? 1231 : 1237);
+		result = prime * result + id;
+		result = prime * result + (isBanned ? 1231 : 1237);
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (canComment != other.canComment)
+			return false;
+		if (id != other.id)
+			return false;
+		if (isBanned != other.isBanned)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password
+				+ ", canComment=" + canComment + ", isBanned=" + isBanned + "]";
+	}
+}
