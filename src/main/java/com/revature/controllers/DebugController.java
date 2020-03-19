23db2fbc62a5fac0986284c69a65501f4651dd4d
Brian Gottreu <gottreu@gmail.com>
2020-03-19 18:40:59 -0500

    Add /populate endpoint
    /populate adds entities to the DB and doesn't require authentication.
    If there's no Users in the DB then there's no passwords to check.

---
+++ src/main/java/com/revature/controllers/DebugController.java
@@ -0,0 +1,45 @@
+package com.revature.controllers;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.Comment;
+import com.revature.models.Image;
+import com.revature.models.User;
+import com.revature.utils.HibernateUtil;
+
+@RestController
+public class DebugController {
+	@GetMapping("/populate")
+	public ResponseEntity<String> populate() {
+		try(Session s = HibernateUtil.getSession()){
+			Transaction tx = s.beginTransaction();
+			//s.save(u);
+			
+			//List<Image> il1 = new Vector<>();
+			//il1.add(i1); il1.add(i2);
+			
+			User u1 = new User(0, "brian", "whatev", false, false, false);
+			User u2 = new User(0, "israel", "password", false, false, true);
+			Image i1 = new Image(0, "https://i.imgur.com/26I32vb.jpg", true, "a wittle kitty", u1);
+			Image i2 = new Image(0, "https://i.imgur.com/XLfaJkK.jpg", false, "TWO KITTIES!!!!", u1);
+			s.save(u1);
+			s.save(u2);
+			s.save(i1); s.save(i2);
+			Comment c1 = new Comment(0, "too few cats", false, u2, i1);
+			Comment c2 = new Comment(0, "too many cats", false, u2, i2);
+			s.save(c1);
+			s.save(c2);
+			
+			tx.commit();
+		}
+		return new ResponseEntity<String>("done", HttpStatus.OK);
+	}
+}
