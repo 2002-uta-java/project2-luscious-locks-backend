b8cc0234ee545915f49486c0c2e5187eac0a0816
Brian Gottreu <gottreu@gmail.com>
2020-03-19 19:02:01 -0500

    Image has a User rather than User having Images
    List<Image> images is removed from User.
    User post is added to Image.

--- src/main/java/com/revature/models/Image.java
+++ src/main/java/com/revature/models/Image.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Image implements Serializable{
@@ -19,16 +21,21 @@ public class Image implements Serializable{
 	@Column(nullable=false)
 	private String url;
 	private boolean isAccepted;
-	
+	private String description;
+	@ManyToOne
+	private User poster;
+
 	public Image() {
 		super();
 	}
 
-	public Image(int id, String url, boolean isAccepted) {
+	public Image(int id, String url, boolean isAccepted, String description, User poster) {
 		super();
 		this.id = id;
 		this.url = url;
 		this.isAccepted = isAccepted;
+		this.description = description;
+		this.poster = poster;
 	}
 
 	public int getId() {
@@ -55,12 +62,30 @@ public class Image implements Serializable{
 		this.isAccepted = isAccepted;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getPoster() {
+		return poster;
+	}
+
+	public void setPoster(User poster) {
+		this.poster = poster;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
 		result = prime * result + id;
 		result = prime * result + (isAccepted ? 1231 : 1237);
+		result = prime * result + ((poster == null) ? 0 : poster.hashCode());
 		result = prime * result + ((url == null) ? 0 : url.hashCode());
 		return result;
 	}
@@ -74,9 +99,19 @@ public class Image implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Image other = (Image) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
 		if (id != other.id)
 			return false;
 		if (isAccepted != other.isAccepted)
+			return false;
+		if (poster == null) {
+			if (other.poster != null)
+				return false;
+		} else if (!poster.equals(other.poster))
 			return false;
 		if (url == null) {
 			if (other.url != null)
@@ -88,7 +123,8 @@ public class Image implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Image [id=" + id + ", url=" + url + ", isAccepted=" + isAccepted + "]";
+		return "Image [id=" + id + ", url=" + url + ", isAccepted=" + isAccepted + ", description="
+				+ description + ", poster=" + poster + "]";
 	}
 	
 	
