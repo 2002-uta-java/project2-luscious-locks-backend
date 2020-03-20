6c35d26ffcc7097712ee1fd360980d7922e7ebfe
Brian Gottreu <gottreu@gmail.com>
2020-03-20 16:21:41 -0500

    Add /login endpoint
    Create a /login endpoint that does not require an
    Authorization header and takes a username and password
    as parameters with a POST request.

--- src/main/java/com/revature/auth/AuthorizationAspect.java
+++ src/main/java/com/revature/auth/AuthorizationAspect.java
@@ -41,7 +41,9 @@ public class AuthorizationAspect {
 	//@Around("within(com.revature.controllers.*) && !within(com.revature.controllers.DebugController.*)")
 	//@Around("within(com.revature.controllers.*) && !within(com.revature.controller.UserController.populate)")
 	@Around("within(com.revature.controllers.*) && !execution(* com.revature.controllers.DebugController.populate())"
-			+ " && !execution(* com.revature.controllers.UserController.createUser(..))")
+			+ " && !execution(* com.revature.controllers.UserController.createUser(..))"
+			+ " && !execution(* com.revature.controllers.UserController.login(..))"
+			)
 	public Object authorizeRequest(ProceedingJoinPoint jp) throws Throwable {
 		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
 				.currentRequestAttributes()).getRequest();
