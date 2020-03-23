cb78a7f15b5f371b1518eb95b45629d5889681a8
Brian Gottreu <gottreu@gmail.com>
2020-03-22 20:28:45 -0500

    createUserTest added

--- src/test/java/com/revature/controllers/UserControllerTest.java
+++ src/test/java/com/revature/controllers/UserControllerTest.java
@@ -23,9 +23,32 @@ public class UserControllerTest {
 	private UserController userController;
 
 	@Test
-	public void createUserTest() {
+	public void getUserTest() {
 		ResponseEntity<User> result =  userController.getUser("1");
 		assertNotNull(result);
 		assertEquals(HttpStatus.OK, result.getStatusCode());
 	}
+	@Test
+	public void createUserTest() {
+		User u = new User(0, "u_745", "x", null,null,null,null);
+		ResponseEntity<String> result = userController.createUser(u);
+		assertNotNull(result);
+		assertEquals(HttpStatus.CREATED, result.getStatusCode());
+		assertNotEquals(0, u.getId());
+	}
+	///////////
+	/*
+		@GetMapping("/users")
+		public ResponseEntity<List<User>> getAllUsers() {
+		@GetMapping("/users/{idOrUsername}")
+		public ResponseEntity<User> getUser(@PathVariable("idOrUsername") String idOrUsername) {
+
+		@PostMapping("/login")
+		public ResponseEntity<User> login(@RequestBody User creds) {
+		@PostMapping("/users")
+		public ResponseEntity<String> createUser(@RequestBody User u) {
+
+		@PutMapping("/users/{id}")
+		public ResponseEntity<User> updateUser(@PathVariable("id") int id, @RequestBody User u) {
+*/
 }
