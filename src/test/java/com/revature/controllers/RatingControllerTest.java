4da8f4a0e76231212c0d0282334b9215a193ce57
Brian Gottreu <gottreu@gmail.com>
2020-03-22 22:46:52 -0500

    src/main/java up to 31% test coverage

--- src/test/java/com/revature/controllers/RatingControllerTest.java
+++ src/test/java/com/revature/controllers/RatingControllerTest.java
@@ -11,7 +11,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.revature.models.Image;
 import com.revature.models.Rating;
+import com.revature.models.User;
+import com.revature.services.ImageService;
+import com.revature.services.UserService;
 import com.revature.test.config.TestBeanConfig;
 
 import javax.servlet.http.HttpSession;
@@ -25,14 +29,24 @@ public class RatingControllerTest {
 	
 	@Autowired
 	private RatingController ratingController;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private ImageService imageService;
 	
 	@Test
 	public void createRatingTest() {
 		HttpSession session = new MockHttpSession();
+		//User u = new User(0, "u_createRating", "x", null,null,null,null);
+		//userService.createUser(u);
+		//assertNotEquals(0, u.getId());
+		Image i = new Image(0, "url", null, "desc", null, null);
+		imageService.createImage(i);
+		assertNotEquals(0, i.getId());
 		
 		Rating r = new Rating();
 		r.setRating(3f);
-		ResponseEntity<String> result = ratingController.createRating(r, 1, session);
+		ResponseEntity<String> result = ratingController.createRating(r, i.getId(), session);
 		assertNotNull(result);
 		assertEquals(HttpStatus.CREATED, result.getStatusCode());
 		result = ratingController.createRating(new Rating(), 0, session);
